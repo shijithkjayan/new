0323d2b8d4d74773d6940d682c4106aed9c3a012
Shijith K <sk@Shijiths-MacBook-Pro.local>
2018-06-27 14:18:35 +0530

    initial commit

---
+++ lib/weather.ex
@@ -0,0 +1,18 @@
+defmodule WEATHER do
+  @moduledoc """
+  Documentation for WEATHER.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WEATHER.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
